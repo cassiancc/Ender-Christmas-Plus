bceda87f4dc76ec027a50793053a2053f5f28dbd
Cassian <cassian@godsted.com>
2026-03-24 16:08:24 -0400

    Port to release 26.1

--- src/main/java/cc/cassian/ender_christmas/mixins/SheetsMixin.java
+++ src/main/java/cc/cassian/ender_christmas/mixins/SheetsMixin.java
@@ -3,7 +3,7 @@ package cc.cassian.ender_christmas.mixins;
 import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.blockentity.state.ChestRenderState;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.resources.model.SpriteId;
+import net.minecraft.client.resources.model.sprite.SpriteId;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.level.block.state.properties.ChestType;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,23 +18,15 @@ import static net.minecraft.client.renderer.blockentity.ChestRenderer.xmasTextur
 @Mixin(Sheets.class)
 public abstract class SheetsMixin {
 
-	@Inject(cancellable = true, at = @At("RETURN"), method = "chooseSprite(Lnet/minecraft/client/renderer/blockentity/state/ChestRenderState$ChestMaterialType;Lnet/minecraft/world/level/block/state/properties/ChestType;)Lnet/minecraft/client/resources/model/SpriteId;")
+	@Inject(cancellable = true, at = @At("RETURN"), method = "chooseSprite")
 	private static void enderChristmas$chooseMaterial(ChestRenderState.ChestMaterialType variant, ChestType type, CallbackInfoReturnable<SpriteId> cir) {
 		boolean christmas = xmasTextures();
 		if (variant.equals(ChestRenderState.ChestMaterialType.ENDER_CHEST) && christmas) {
 			cir.setReturnValue(CHEST_MAPPER.apply(Identifier.fromNamespaceAndPath("ender_christmas", "entity/chest/ender_christmas")));
 		}
 		if (variant.equals(ChestRenderState.ChestMaterialType.REGULAR) && christmas) {
-			cir.setReturnValue(getChestTextureId(type, CHEST_XMAS_LOCATION, CHEST_XMAS_LOCATION_LEFT, CHEST_XMAS_LOCATION_RIGHT));
+			cir.setReturnValue(CHEST_CHRISTMAS.select(type));
 		}
-	}
-
-	private static SpriteId getChestTextureId(ChestType type, SpriteId single, SpriteId left, SpriteId right) {
-        return switch (type) {
-            case LEFT -> left;
-            case RIGHT -> right;
-            default -> single;
-        };
 	}
 
 
